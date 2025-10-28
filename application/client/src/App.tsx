8fcf59169d1cef4ba245e7c27e066de10dc5b2bd
[Hakeeb Adekoya] <[adekoyahakeeb@gmail.com]>
2025-10-28 16:45:02 -0400

    Protected Routes that redirects to login page

--- application/client/src/App.tsx
+++ application/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
+import  { useState } from "react";
 import Nav from './components/Nav';
 import HomePage from './components/Homepage';
 import Registration from './components/Registration';
@@ -10,12 +11,17 @@ import ProfileForm from './components/ProfileForm';
 import Logout from './components/Logout';
 import Education from './components/Education/Education';
 import AuthCallback from './components/AuthCallback';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState<boolean>(
+      () => !!localStorage.getItem("authToken")
+    );
   const location = useLocation();
   const hideNavbarRoutes = ["/Login", "/Registration"];
   const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+  
   return (
     <>
       {showNavbar && <Nav />}
@@ -24,13 +30,13 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/Registration" element={<Registration />} />
           <Route path="/auth/callback" element={<AuthCallback />} /> 
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Dashboard" element={ <PrivateRoute><Dashboard /></PrivateRoute>} /> {/* Protected Routes */}
+          <Route path="/Skills" element={<PrivateRoute><Skills /></PrivateRoute>} />{/* Protected Routes */}
           <Route path="/Login" element={<LoginPage />} />
-          <Route path="/ProfilePage" element={<ProfilePage />} />
-          <Route path="/ProfileForm" element={<ProfileForm />} />
+          <Route path="/ProfilePage" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />{/* Protected Routes */}
+          <Route path="/ProfileForm" element={<PrivateRoute><ProfileForm /></PrivateRoute>} />{/* Protected Routes */}
           <Route path="/Logout" element={<Logout />} />
-          <Route path="/Education" element={<Education />} />
+          <Route path="/Education" element={<PrivateRoute><Education /></PrivateRoute>} />{/* Protected Routes */}
         </Routes>
       </div>
     </>
